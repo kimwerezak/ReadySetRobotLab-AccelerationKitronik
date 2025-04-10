bc93024688c6292dfad106c0cbd0b6584ad839d1
Kimberley Werezak <122053493+kimwerezak@users.noreply.github.com>
2025-04-10 14:12:44 -0600

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
+#This will move the position of the servo to create tick marks to measure velocity
 def move_servo(position_1, position_2, time_1, time_2): 
     pins.servo_write_pin(AnalogPin.P15, position_1)
     basic.pause(time_1)
     pins.servo_write_pin(AnalogPin.P15, position_2)
     basic.pause(time_2)
-        
-def accelerate_movement(speed, delay, accelerations): 
+
+#This will accelerate the robot and leave a tick mark with each acceleration.        
+def accelerate_movement(speed, accelerations): 
     Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, speed)
-    for index in range(accelerations):
-        move_servo(80,135,50,950)
-        basic.pause(delay)
+    for i in range(accelerations):
+        move_servo(0, 180, 200, 800)
         speed += 5
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, speed)
     Kitronik_Move_Motor.stop()
 
+#The main function will accelerate the robot, starting at speed 10 for 6 accelerations,
+#leaving a tick with each acceleration.
 def main():
-    accelerate_movement(10,500,9)
+    accelerate_movement(10, 6)
 
+#Touch the logo on the MicroBit to start the code.
 input.on_logo_event(TouchButtonEvent.PRESSED, main)
 
--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+// This will move the position of the servo to create tick marks to measure velocity
 function move_servo(position_1: number, position_2: number, time_1: number, time_2: number) {
     pins.servoWritePin(AnalogPin.P15, position_1)
     basic.pause(time_1)
@@ -5,17 +6,20 @@ function move_servo(position_1: number, position_2: number, time_1: number, time
     basic.pause(time_2)
 }
 
-function accelerate_movement(speed: number, delay: number, accelerations: number) {
+// This will accelerate the robot and leave a tick mark with each acceleration.        
+function accelerate_movement(speed: number, accelerations: number) {
     Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
-    for (let index = 0; index < accelerations; index++) {
-        move_servo(80, 135, 50, 950)
-        basic.pause(delay)
+    for (let i = 0; i < accelerations; i++) {
+        move_servo(0, 180, 200, 800)
         speed += 5
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
     }
     Kitronik_Move_Motor.stop()
 }
 
+// The main function will accelerate the robot, starting at speed 10 for 6 accelerations,
+// leaving a tick with each acceleration.
+// Touch the logo on the MicroBit to start the code.
 input.onLogoEvent(TouchButtonEvent.Pressed, function main() {
-    accelerate_movement(10, 500, 9)
+    accelerate_movement(10, 6)
 })
